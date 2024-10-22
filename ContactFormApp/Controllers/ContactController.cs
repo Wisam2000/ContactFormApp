aba7b395885f24a76f4b074136b32c8c27fac356
Wisam2000 <wsa2000000@hotmail.com>
2024-10-22 12:14:14 +0300

    Add project files.

---
+++ ContactFormApp/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContactFormApp.Models;
+
+namespace ContactFormApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                // You can add code here to save the data or send an email, etc.
+                ViewBag.Message = "Your contact information has been submitted successfully!";
+                return View(contact);
+            }
+            return View();
+        }
+    }
+}
---
+++ ContactFormApp/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactFormApp.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^07[789]\d{7}$", ErrorMessage = "Phone number must start with '077', '078', or '079' and be exactly 10 digits long.")]
+        [StringLength(10, ErrorMessage = "Phone number can't exceed 10 digits.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Message is required.")]
+        [StringLength(500, ErrorMessage = "Message can't exceed 500 characters.")]
+        public string Message { get; set; }
+    }
+}
